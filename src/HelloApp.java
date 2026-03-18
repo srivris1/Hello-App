2aa6d23f796e89a3edf71dcce19d7e50515f20fe
Rishit Srivastava <srivris55@gmail.com>
2026-03-18 18:40:10 +0530

    UC4 HEllo

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -2,10 +2,17 @@ public class HelloApp {
 
     public static void main(String[] args) {
 
-        // Assign name from argument or default to "World"
-        String name = (args.length > 0) ? args[0] : "World";
+        // Case 1: No arguments → default "World"
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } 
+        // Case 2: One or more arguments
+        else {
+            // Join all names with comma and space
+            String names = String.join(", ", args);
 
-        // Display greeting
-        System.out.println("Hello, " + name + "!");
+            // Print greeting
+            System.out.println("Hello, " + names + "!");
+        }
     }
 }
